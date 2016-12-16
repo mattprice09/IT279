b5aae5a306a10e4fcd1b156a5b4dc2a42c03ce36
mattprice09 <mattprice09@rocketmail.com>
2016-12-15 18:36:26 -0600

    Data structures

--- asg4/WDGraph.h
+++ asg4/WDGraph.h
@@ -6,14 +6,35 @@
 #define WDGRAPH_H
 
 using namespace std;
+#include <map>
+#include <string>
 #include <utility>
 #include <vector>
 
+
+
 class WDGraph {
  private:
+  // Map node names to node objects so that we can efficiently get information on a vertex
+  map<string, node> nodes;
+  // Each node is an item in the adjacency list containing a vector of edges (represented by pairs).
+  // pair.first -> the destination node
+  // pair.second -> the destination weight
   vector< vector< pair<int, int>>> adjacencyList;
 
  public:
+
+  // Represents a vertex in the graph.
+  class node {
+  public:
+
+    node();
+    node(string n);
+
+    string name;
+    int key;
+    bool visited;
+  }
 
   WDGraph();
   // Creates an empty WDGraph;
@@ -23,6 +44,16 @@ class WDGraph {
 
   virtual ~WDGraph();
   // Destructor
+
+  void addVertex(string name);
+
+  void addEdge(string source, string dest, int weight);
+
+  void topologicalSort();
+
+  void shortestPath(string sourceNode);
+
+  void minSpanTree();
 
  private:
 
